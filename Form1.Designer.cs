2b723005e4cd081b8514425c28e249cd14b1fdf2
Administrator <Administrator@DESKTOP-KV5USTP>
2021-08-25 01:29:24 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace 提取链接
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.rtb = new System.Windows.Forms.RichTextBox();
+            this.rtb1 = new System.Windows.Forms.RichTextBox();
+            this.btnGetCopy = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btn = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.Tb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnM = new System.Windows.Forms.Button();
+            this.ls = new System.Windows.Forms.Label();
+            this.btnBD = new System.Windows.Forms.Button();
+            this.btn250 = new System.Windows.Forms.Button();
+            this.Btn25 = new System.Windows.Forms.Button();
+            this.BtnGetTxt = new System.Windows.Forms.Button();
+            this.RtbTxtList = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Tb1 = new System.Windows.Forms.TextBox();
+            this.BtnCc = new System.Windows.Forms.Button();
+            this.cb = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // rtb
+            // 
+            this.rtb.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rtb.Location = new System.Drawing.Point(12, 12);
+            this.rtb.Name = "rtb";
+            this.rtb.Size = new System.Drawing.Size(708, 393);
+            this.rtb.TabIndex = 0;
+            this.rtb.Text = "";
+            // 
+            // rtb1
+            // 
+            this.rtb1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rtb1.Location = new System.Drawing.Point(740, 12);
+            this.rtb1.Name = "rtb1";
+            this.rtb1.Size = new System.Drawing.Size(708, 674);
+            this.rtb1.TabIndex = 1;
+            this.rtb1.Text = "";
+            // 
+            // btnGetCopy
+            // 
+            this.btnGetCopy.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGetCopy.Location = new System.Drawing.Point(740, 725);
+            this.btnGetCopy.Name = "btnGetCopy";
+            this.btnGetCopy.Size = new System.Drawing.Size(249, 44);
+            this.btnGetCopy.TabIndex = 2;
+            this.btnGetCopy.Text = "提取并复制";
+            this.btnGetCopy.UseVisualStyleBackColor = true;
+            this.btnGetCopy.Click += new System.EventHandler(this.btnGetCopy_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCopy.Location = new System.Drawing.Point(1150, 725);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(131, 44);
+            this.btnCopy.TabIndex = 3;
+            this.btnCopy.Text = "复制";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btn
+            // 
+            this.btn.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn.Location = new System.Drawing.Point(1017, 725);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(105, 44);
+            this.btn.TabIndex = 4;
+            this.btn.Text = "提取";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClean.Location = new System.Drawing.Point(1309, 725);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(131, 44);
+            this.btnClean.TabIndex = 5;
+            this.btnClean.Text = "清空";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // Tb
+            // 
+            this.Tb.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tb.Location = new System.Drawing.Point(277, 734);
+            this.Tb.Name = "Tb";
+            this.Tb.Size = new System.Drawing.Size(443, 35);
+            this.Tb.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 737);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "链接都有的文字：";
+            // 
+            // btnM
+            // 
+            this.btnM.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnM.Location = new System.Drawing.Point(436, 786);
+            this.btnM.Name = "btnM";
+            this.btnM.Size = new System.Drawing.Size(284, 44);
+            this.btnM.TabIndex = 8;
+            this.btnM.Text = "提取磁力链接并复制";
+            this.btnM.UseVisualStyleBackColor = true;
+            this.btnM.Click += new System.EventHandler(this.btnM_Click);
+            // 
+            // ls
+            // 
+            this.ls.AutoSize = true;
+            this.ls.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ls.Location = new System.Drawing.Point(23, 796);
+            this.ls.Name = "ls";
+            this.ls.Size = new System.Drawing.Size(22, 24);
+            this.ls.TabIndex = 9;
+            this.ls.Text = "0";
+            // 
+            // btnBD
+            // 
+            this.btnBD.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBD.Location = new System.Drawing.Point(740, 786);
+            this.btnBD.Name = "btnBD";
+            this.btnBD.Size = new System.Drawing.Size(249, 44);
+            this.btnBD.TabIndex = 10;
+            this.btnBD.Text = "提取百度网盘链接";
+            this.btnBD.UseVisualStyleBackColor = true;
+            this.btnBD.Click += new System.EventHandler(this.btnBD_Click);
+            // 
+            // btn250
+            // 
+            this.btn250.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn250.Location = new System.Drawing.Point(1017, 786);
+            this.btn250.Name = "btn250";
+            this.btn250.Size = new System.Drawing.Size(105, 44);
+            this.btn250.TabIndex = 11;
+            this.btn250.Text = "250更新";
+            this.btn250.UseVisualStyleBackColor = true;
+            this.btn250.Click += new System.EventHandler(this.btn250_Click);
+            // 
+            // Btn25
+            // 
+            this.Btn25.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Btn25.Location = new System.Drawing.Point(1150, 786);
+            this.Btn25.Name = "Btn25";
+            this.Btn25.Size = new System.Drawing.Size(131, 44);
+            this.Btn25.TabIndex = 12;
+            this.Btn25.Text = "去除250";
+            this.Btn25.UseVisualStyleBackColor = true;
+            this.Btn25.Click += new System.EventHandler(this.Btn25_Click);
+            // 
+            // BtnGetTxt
+            // 
+            this.BtnGetTxt.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnGetTxt.Location = new System.Drawing.Point(1309, 786);
+            this.BtnGetTxt.Name = "BtnGetTxt";
+            this.BtnGetTxt.Size = new System.Drawing.Size(131, 44);
+            this.BtnGetTxt.TabIndex = 13;
+            this.BtnGetTxt.Text = "获取TXT";
+            this.BtnGetTxt.UseVisualStyleBackColor = true;
+            this.BtnGetTxt.Click += new System.EventHandler(this.BtnGetTxt_Click);
+            // 
+            // RtbTxtList
+            // 
+            this.RtbTxtList.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.RtbTxtList.Location = new System.Drawing.Point(12, 411);
+            this.RtbTxtList.Name = "RtbTxtList";
+            this.RtbTxtList.Size = new System.Drawing.Size(708, 275);
+            this.RtbTxtList.TabIndex = 14;
+            this.RtbTxtList.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(113, 786);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 44);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "获取已存在";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Tb1
+            // 
+            this.Tb1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tb1.Location = new System.Drawing.Point(277, 693);
+            this.Tb1.Name = "Tb1";
+            this.Tb1.Size = new System.Drawing.Size(443, 35);
+            this.Tb1.TabIndex = 16;
+            // 
+            // BtnCc
+            // 
+            this.BtnCc.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnCc.Location = new System.Drawing.Point(277, 786);
+            this.BtnCc.Name = "BtnCc";
+            this.BtnCc.Size = new System.Drawing.Size(127, 44);
+            this.BtnCc.TabIndex = 17;
+            this.BtnCc.Text = "查重";
+            this.BtnCc.UseVisualStyleBackColor = true;
+            this.BtnCc.Click += new System.EventHandler(this.BtnCc_Click);
+            // 
+            // cb
+            // 
+            this.cb.FormattingEnabled = true;
+            this.cb.Items.AddRange(new object[] {
+            "文件模式",
+            "文本模式"});
+            this.cb.Location = new System.Drawing.Point(33, 702);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(121, 25);
+            this.cb.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1476, 841);
+            this.Controls.Add(this.cb);
+            this.Controls.Add(this.BtnCc);
+            this.Controls.Add(this.Tb1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.RtbTxtList);
+            this.Controls.Add(this.BtnGetTxt);
+            this.Controls.Add(this.Btn25);
+            this.Controls.Add(this.btn250);
+            this.Controls.Add(this.btnBD);
+            this.Controls.Add(this.ls);
+            this.Controls.Add(this.btnM);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Tb);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnGetCopy);
+            this.Controls.Add(this.rtb1);
+            this.Controls.Add(this.rtb);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "提取链接";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb;
+        private System.Windows.Forms.RichTextBox rtb1;
+        private System.Windows.Forms.Button btnGetCopy;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.TextBox Tb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnM;
+        private System.Windows.Forms.Label ls;
+        private System.Windows.Forms.Button btnBD;
+        private System.Windows.Forms.Button btn250;
+        private System.Windows.Forms.Button Btn25;
+        private System.Windows.Forms.Button BtnGetTxt;
+        private System.Windows.Forms.RichTextBox RtbTxtList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Tb1;
+        private System.Windows.Forms.Button BtnCc;
+        private System.Windows.Forms.ComboBox cb;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace 提取链接
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Tb.Text = @"D:\0000文件列表";
+            Tb1.Text = @"H:\00-已压缩";
+        }
+        //string connectString = "Data Source=.;Initial Catalog=a250;Integrated Security=True";
+
+        //提取链接
+        private void btn_Click(object sender, EventArgs e)
+        {
+            foreach (string i in rtb.Lines)
+            {
+                if (i.IndexOf(Tb.Text) != -1)
+                {
+                    rtb1.AppendText(i.Replace(Tb.Text, "") + "\n");
+                }
+            }
+        }
+
+        private void btnGetCopy_Click(object sender, EventArgs e)
+        {
+            string str = Tb.Text.Trim();
+            foreach (string i in rtb.Lines)
+            {
+                if (i.IndexOf(str) != -1)
+                {
+                    rtb1.AppendText(i.Replace(str, "") + "\n");
+                }
+            }
+            Clipboard.SetDataObject(rtb1.Text);
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(rtb1.Text);
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            rtb.Text = "";
+            rtb1.Text = "";
+            rtb.Focus();
+        }
+
+        //提取磁力链接
+        private void btnM_Click(object sender, EventArgs e)
+        {
+            //rtb1.AppendText("D68D4564267F5B78C3A161A361F063611DEFE3F6".Length.ToString());
+            int sum = 0;
+            foreach (string i in rtb.Lines)
+            {
+                if (i.IndexOf(Tb.Text) != -1)
+                {
+                    string t = i.Trim().Replace(Tb.Text, "");
+                    // rtb1.AppendText(t.Length + "\n"+t);
+                    if (t.Length == 40 && System.Text.RegularExpressions.Regex.IsMatch(t, @"^[A-Za-z0-9]{4,40}$"))
+                    {
+                        rtb1.AppendText(t + "\n");
+                        sum++;
+                    }
+                }
+                if (i.Trim().Length == 40 && System.Text.RegularExpressions.Regex.IsMatch(i.Trim(), @"^[A-Za-z0-9]+$"))
+                {
+                    rtb1.AppendText(i + "\n");
+                    sum++;
+                }
+            }
+            ls.Text = (Convert.ToInt32(ls.Text) + sum).ToString();
+            Clipboard.SetDataObject(rtb1.Text);
+        }
+
+        private void btnBD_Click(object sender, EventArgs e)
+        {
+            if (cb.SelectedIndex==1)
+            {
+                int sum = 0;
+                foreach (string i in rtb.Lines)
+                {
+                    int index = i.IndexOf("https://pan.baidu.com/s/");
+                    if (index != -1)
+                    {
+                        string s = "https://pan.baidu.com/s/";
+                        for (int j = index; j < i.Length; j++)
+                        {
+                            s += i[j];
+                        }
+                        rtb1.AppendText(s + "\n");
+                        sum++;
+                    }
+                }
+                ls.Text = (Convert.ToInt32(ls.Text) + sum).ToString();
+                Clipboard.SetDataObject(rtb1.Text); 
+            }
+            else
+            {
+                RtbTxtList.Text = "";
+                Tb.Text = Tb.Text.Trim();
+                if (Tb.Text.LastIndexOf("\\") != Tb.Text.Length - 1)//把c: 变成c:\，即在最后添上\
+                {
+                    Tb.Text += "\\";
+                }
+                string sourcepath = Tb.Text;
+                if (!Directory.Exists(sourcepath))
+                {
+                    return;
+                }
+                string[] files = Directory.GetFiles(sourcepath, "*.txt"); //获取指定目录所有txt文件的绝对路径
+                foreach (string txtfile in files)//列出每一个txt文件
+                {
+                    RtbTxtList.AppendText(txtfile + "\n");
+                    //RtbTxtList.ScrollToCaret();
+                    StreamReader list = new(txtfile);
+                    string i;
+                    while ((i = list.ReadLine()) != null)//读取每个TXT的每一行
+                    {
+                        rtb.AppendText(i + "\n");
+                        string bdlink = "https://pan.baidu.com/s/";
+                        int index = i.IndexOf(bdlink);
+                        if (index != -1)
+                        {
+                            for (int j = index; j < i.Length; j++)
+                            {
+                                bdlink += i[j];
+                            }
+                            rtb1.AppendText(bdlink + "\n");
+                        }
+                    }
+                    list.Close();
+                    string txtpath = Directory.GetParent(txtfile).FullName + "\\";
+                    string path = txtpath + Path.GetFileNameWithoutExtension(txtfile) + @"---百度网盘.txt";
+                    //创建 FileStream 类的实例
+                    FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                    string msg = rtb1.Text;
+                    byte[] bytes = Encoding.UTF8.GetBytes(msg);
+                    fileStream.Write(bytes, 0, bytes.Length);
+                    fileStream.Flush();
+                    fileStream.Close();
+                    rtb1.Text = "";
+                }
+            }
+        }
+
+        //检测是否重复
+        private void btn250_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        //去除250网址
+        private void Btn25_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            foreach (string i in rtb.Lines)
+            {
+                if (i.Contains("http://250a") && i != "")
+                {
+                    rtb1.AppendText(i + "\n");
+                    if (i.Contains("https://pan.baidu.com/s/"))
+                    {
+                        sum++;
+                    }
+                }
+            }
+            ls.Text = (Convert.ToInt32(ls.Text) + sum).ToString();
+            Clipboard.SetDataObject(rtb1.Text);
+        }
+
+        private void BtnGetTxt_Click(object sender, EventArgs e)
+        {
+            Tb.Text = Tb.Text.Trim();
+            if (Tb.Text.LastIndexOf("\\") != Tb.Text.Length - 1)//把c: 变成c:\，即在最后添上\
+            {
+                Tb.Text += "\\";
+            }
+            RtbTxtList.Text = "";
+            string sourcepath = Tb.Text;
+            if (!Directory.Exists(sourcepath))
+            {
+                return;
+            }
+            string[] files = Directory.GetFiles(sourcepath, "*.txt");            
+            foreach (string txtfile in files)//txt文件列表
+            {
+                RtbTxtList.AppendText(txtfile + "\n");
+                //rtb1.AppendText(txtfile + "\n");
+                StreamReader list = new(txtfile);
+                string ke;
+                int num = 3;
+                string a = "";
+                string b = "";
+                {
+                    //SqlConnection sqlCnt = new SqlConnection(connectString);
+                    //sqlCnt.Open();
+                    //SqlCommand command = new SqlCommand();
+                    //command.Connection = sqlCnt;            // 绑定SqlConnection对象
+                    //SqlCommand cmd = sqlCnt.CreateCommand();              //创建SqlCommand对象
+                    //cmd.CommandType = CommandType.Text;
+                    //cmd.CommandText = "select * from products = @ID";   //sql语句
+                    //cmd.Connection = sqlCnt;
+                    //cmd.Parameters.Add("@ID", SqlDbType.Int);
+                    //cmd.Parameters["@ID"].Value = 1;                    //给参数sql语句的参数赋值
+                    // 创建SqlDataAdapter
+                    //SqlDataAdapter myDataAdapter = new SqlDataAdapter();
+                    //myDataAdapter.SelectCommand = cmd;	// 为SqlDataAdapter对象绑定所要执行的SqlCommand对象
+                    //cmd.ExecuteNonQuery();
+                }
+
+                int flag = 0;
+                while ((ke = list.ReadLine()) != null)//TXT的每一行是一节课
+                {
+                    //rtb1.AppendText(num+ ke + "\n");
+                    if (num == 3)
+                    {
+                        a = ke;
+                        num--;
+                    }
+                    else if (num == 2)
+                    {
+                        num--;
+                                b = ke;
+                        foreach (string i in rtb.Lines)//从rtb里读每一行与TXT每一行比较
+                        {
+                            if (string.Equals(i, ke))
+                            {
+                                flag = 1;//匹配上了
+                                //rtb1.AppendText(i + ke + "已存在\n");
+                                break;
+                            }
+                        }
+                    }
+                    else if (num == 1)
+                    {
+                        num = 3;
+                        if (flag == 0)
+                        {
+                            rtb1.AppendText(a + "\n" + b + "\n" + ke + "\n");
+                        }
+                        flag = 0;
+                    }
+                }
+                list.Close();
+                //rtb1.AppendText(i + "\n");
+                string txtpath = Directory.GetParent(txtfile).FullName+"\\";
+                string path = txtpath+Path.GetFileNameWithoutExtension( txtfile)+ @"---.txt";
+                //创建 FileStream 类的实例
+                FileStream fileStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                //定义学号
+                string msg = rtb1.Text;
+                //将字符串转换为字节数组
+                byte[] bytes = Encoding.UTF8.GetBytes(msg);
+                //向文件中写入字节数组
+                fileStream.Write(bytes, 0, bytes.Length);
+                //刷新缓冲区
+                fileStream.Flush();
+                //关闭流
+                fileStream.Close();
+                rtb1.Text = "";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Tb1.Text = Tb1.Text.Trim();
+            if (Tb1.Text.LastIndexOf("\\") != Tb1.Text.Length - 1)//把c: 变成c:\，即在最后添上\
+            {
+                Tb1.Text += "\\";
+            }
+            RtbTxtList.Text = "";
+            string sourcepath = Tb1.Text;
+            if (!Directory.Exists(sourcepath))
+            {
+                return;
+            }
+            string[] flods = Directory.GetDirectories(sourcepath);
+            foreach (string i in flods) //列出每一个文件夹
+            {
+                string flod = Path.GetFileName(i);
+                rtb.AppendText(flod + "\n");
+            }
+            string[] files = Directory.GetFiles(sourcepath, "*.txt");
+            foreach (string i in files) //列出每一个文件
+            {
+                string file = Path.GetFileNameWithoutExtension(i);
+                rtb.AppendText(file + "\n");
+            }
+        }
+
+        private void BtnCc_Click(object sender, EventArgs e)
+        {
+            foreach (string i in rtb.Lines)
+            {
+                foreach (string j in RtbTxtList.Lines)
+                {
+                    if (string.Equals(i,j))
+                    {
+                        rtb1.AppendText(i+"\n");
+                    }
+                }
+            }
+        }
+    }
+}
+
